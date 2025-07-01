e7212b3fe8af01b1fd99b6c0d4d071f22b1303f1
Tyler Ho <mytylerho@gmail.com>
2025-07-01 10:44:51 -0700

    Added access to song class in file_processing.

---
+++ file_processing/song.py
@@ -0,0 +1,11 @@
+class Song:
+    def __init__(self, path: str, company: str, generation: str, artist: str, name: str):
+        self.path = path
+        self.company = company
+        self.generation = generation
+        self.artist = artist
+        self.name = name
+
+    def __repr__(self):
+        return (f"Song(path={self.path!r}, company={self.company!r}, generation={self.generation!r}, "
+                f"artist={self.artist!r}, name={self.name!r})")
