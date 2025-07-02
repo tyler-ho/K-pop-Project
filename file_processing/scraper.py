a4250627e9cc5eba941621d8212c3d775899fb0c
Tyler Ho <mytylerho@gmail.com>
2025-07-02 14:18:10 -0700

    Cleaned up the functionality.

--- file_processing/scraper.py
+++ file_processing/scraper.py
@@ -1,7 +1,19 @@
 import yt_dlp
 import os
-import re
+import logging
+import pandas as pd
+from typing import List
 from urllib.parse import urlparse, parse_qs
+
+# need logging to see the progress 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s — %(levelname)s — %(message)s",
+    handlers=[
+        logging.FileHandler("/home/users/tylerho/kpop_project/K-pop-Project/logs/scrape_without_dupes.log"),
+        logging.StreamHandler()
+    ]
+)
 
 def is_youtube_music_url(url):
     """Check if the URL is from YouTube Music"""
@@ -27,6 +39,14 @@ def get_playlist_from_artist(artist_url):
         
         return playlists
 
+BANNED_WORDS = ['live', 'tour', 'inst', 'korean', 'chinese', 'japanese', 'thai', 'remix', 'version'] # remove any concert recordings, instrumentals, copied songs in other languages, and remixes
+def has_banned_words(title: str, banned_words: List[str]=BANNED_WORDS):
+    for banned_word in banned_words:
+        if banned_word in title.lower():
+            return True
+    return False
+
+
 def download_track(track_url, output_dir="downloads"):
     """Download a single track"""
     ydl_opts = {
@@ -40,16 +60,21 @@ def download_track(track_url, output_dir="downloads"):
         'ignoreerrors': True,
         'quiet': False,
         'keepvideo': False,
-        'download_archive': 'downloaded_videos.txt',
+        'download_archive': 'logs/downloaded_videos.txt',
         'no_overwrites': True
     }
     
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            title = ydl.extract_info(track_url, download=False).get('title')
+            if has_banned_words(title):
+                logging.info(f'Skipped {title}.')
+                return True
+            logging.info(f"Downloading {title}.")
             ydl.download([track_url])
         return True
     except Exception as e:
-        print(f"Error downloading {track_url}: {e}")
+        logging.info(f"Error downloading {track_url}: {e}")
         return False
 
 def download_from_youtube_music(url, output_dir="downloads"):
@@ -58,7 +83,7 @@ def download_from_youtube_music(url, output_dir="downloads"):
         os.makedirs(output_dir)
     
     if not is_youtube_music_url(url):
-        print("Not a YouTube Music URL")
+        logging.info("Not a YouTube Music URL")
         return
     
     parsed_url = urlparse(url)
@@ -66,30 +91,22 @@ def download_from_youtube_music(url, output_dir="downloads"):
     
     # Handle different URL types
     if '/channel/' in path or '/artist/' in path:
-        print(f"Processing artist: {url}")
+        logging.info(f"Processing artist: {url}")
         playlists = get_playlist_from_artist(url)
         
-        for playlist_url in playlists:
-            print(f"Processing playlist: {playlist_url}")
-            download_track(playlist_url, output_dir)
-    
-    elif '/playlist' in path:
-        print(f"Processing playlist: {url}")
-        download_track(url, output_dir)
-    
-    elif '/watch' in path:
-        print(f"Processing single track: {url}")
-        download_track(url, output_dir)
-    
+        for song_url in playlists:
+            logging.info(f"Processing song: {song_url}")
+            download_track(song_url, output_dir)
+
     else:
-        print(f"Unsupported URL type: {url}")
+        logging.info(f"Unsupported URL type: {url}")
 
 # Example usage
-# if __name__ == "__main__":
-#     artist_url = "https://music.youtube.com/channel/UCkbbMCA40i18i7UdjayMPAg"
-#     download_from_youtube_music(artist_url, "audio_output_viachannel/Blackpink")
+if __name__ == "__main__":
+    BANNED_WORDS = ['live', 'tour', 'inst', 'korean', 'chinese', 'japanese', 'thai', 'remix', 'version'] 
+    data_path = 'file_processing/DownloadViaChannel.xlsx'
+    df = pd.read_excel(data_path)
+    for row in df.itertuples():
+        download_from_youtube_music(row.group_link, "music_downloads/" + row.company + '/' + str(row.generation) + '/' + row.group)
 
 
-
-# pull_playlist("https://music.youtube.com/playlist?list=OLAK5uy_km8YKnQCFd510fbMib2ZhDKYWKWvz_qls", "5", "Xdinary Heroes")
-# pull_all_songs_from_channel("https://music.youtube.com/channel/UCkbbMCA40i18i7UdjayMPAg", "YG", "Blackpink", "3")
