ba4d25dd2daaa1182a00c835ab3889ee04878f66
tyler-ho <mytylerho@gmail.com>
2025-07-28 17:12:01 -0700

    Added more constants.

--- classes/constants.py
+++ classes/constants.py
@@ -1,3 +1,14 @@
-N_JOBS = 4 # number of threads to create; used for multi-threading
+from file_processing.create_clip_embeddings import get_feature_labels
+
+# Ignore/remove copied songs in other languages, instrumentals, remixes, and
+# concert recordings.
+BANNED_WORDS = ['korean', 'kor', 'chinese', 'japanese', 'jpn', 'thai',
+                'inst', 'karaoke', 'version', 'mix', '~',
+                'live', 'tour']
+FEATURE_LABELS = get_feature_labels() # labels for librosa features
+N_JOBS = 1 # number of threads to create; used for multi-threading
 RANDOM_STATE = 42 # random state; used for reproducibility
 
+# Embedding Types
+VCE = 'vocal_clip_embeddings'
+ICE = 'intrumental_clip_embeddings'
