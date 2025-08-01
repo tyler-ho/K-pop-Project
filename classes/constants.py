785b8e8d887adde1bca9e2ae5853bba18ba21dc7
tyler-ho <mytylerho@gmail.com>
2025-08-01 00:32:56 -0700

    Added support for updating N_JOBS via environment variable.

--- classes/constants.py
+++ classes/constants.py
@@ -1,3 +1,4 @@
+import os
 from file_processing.create_clip_embeddings import get_feature_labels
 
 # Ignore/remove copied songs in other languages, instrumentals, remixes, and
@@ -6,9 +7,12 @@ BANNED_WORDS = ['korean', 'kor', 'chinese', 'japanese', 'jpn', 'thai',
                 'inst', 'karaoke', 'version', 'mix', '~',
                 'live', 'tour']
 FEATURE_LABELS = get_feature_labels() # labels for librosa features
-N_JOBS = 1 # number of threads to create; used for multi-threading
+N_JOBS = 1 # number of threads to create; used for multi-threadinga
+if int(os.getenv('N_JOBS', default=1)) > 1:
+    N_JOBS = int(os.getenv('N_JOBS', default=1))
 RANDOM_STATE = 42 # random state; used for reproducibility
 
 # Embedding Types
 VCE = 'vocal_clip_embeddings'
 ICE = 'intrumental_clip_embeddings'
+STEMS = ['drums', 'bass', 'other', 'vocals']
